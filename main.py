5295fa2b3353accc6d7e1a7a723ab5b0a8394f02
subham chakraborty <chakrabortysubham448@gmail.com>
2026-08-08 12:54:20 +0530

    add create account method

--- main.py
+++ main.py
@@ -16,9 +16,35 @@ class Bank:
     except Exception as e:
         print(f"Error loading data: {e}")
     
+    
+    #update database
+    @staticmethod
+    def update():
+        with open(Bank.database, 'w') as f:
+           f.write(json.dumps(Bank.data))
+           
+           
+           
     #create account
     def create_account(self):
-        pass
+        data = {
+            "name": input("Enter your name:- "),
+            "age":int(input("Enter your age:- ")),
+            "email": input("Enter your email:- "),
+            "pin":int(input("Enter your four digit pin:- ")),
+            "account_number":1234,
+            "balance":0
+        }
+        if data["age"] < 18 or len(str(data["pin"])) != 4:
+            print("You are not eligible for creating an account.")
+        else:
+            print("Account created successfully.")
+            for i in data:
+                print(f"{i}: {data[i]}")
+            print("Please remember your account number and pin for future transactions.")
+            
+            Bank.data.append(data)
+            Bank.update()
 
 
 user = Bank()
